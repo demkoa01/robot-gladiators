37220b43f83ee46dfd49cb7b10e4a9c78014806b
Allison Demkovich <demkovich.allison@gmail.com>
2021-12-06 15:12:19 -0600

    add console to JS

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
-window.alert("This is an alert! JavaScript is running!");
+// prompt player to enter a name & save as variable
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 +10);
+// what is this? - Output below + name
+console.log("Our robot's name is " + playerName);
+
+// create a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
